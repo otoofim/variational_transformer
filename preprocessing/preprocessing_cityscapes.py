789a6a8e16d38c9edeab225876ea907ae953e3d8
otoofim <moh1371@gmail.com>
2021-12-21 18:21:13 +0000

    modified preprocessing

--- preprocessing/preprocessing_cityscapes.py
+++ preprocessing/preprocessing_cityscapes.py
@@ -1,15 +1,66 @@
+import sys
+import os
+import glob
+from PIL import Image
+import numpy as np
+from torchvision import transforms
+import random
+import torch
+import torchvision.transforms.functional as TF
 
+
+class RandomErasing:
+
+    def __call__(self, x):
+
+        if not isinstance(x, torch.Tensor):
+            x = transforms.ToTensor()(x)
+        x = transforms.ToPILImage()(transforms.RandomErasing(p=1.)(x))
+        return x
+
+class RandomAffine:
+
+    def __init__(self, angles = (0,360), translate = (2,2), scale = (0,2), shear = (-180,180)):
+        self.angle_range = angles
+        self.translate = translate
+        self.scale_range = scale
+        self.shear_range = shear
+        self.__new_seed__()
+
+    def __call__(self, x):
+        return TF.affine(x, self.angle, self.translate, self.scale, self.shear)
+
+    def __new_seed__(self):
+        self.angle = random.uniform(*self.angle_range)
+        self.scale = random.uniform(*self.scale_range)
+        self.shear = random.uniform(*self.shear_range)
+
+
+customized_augment_transforms = [
+    transforms.ColorJitter(brightness = (0.5,2), contrast = (0.5,2), saturation = (0.5,2), hue = (-0.5,0.5)),
+    RandomErasing(),
+    transforms.GaussianBlur(9),
+    transforms.Grayscale(num_output_channels = 3),
+    RandomAffine(angles = (0,360), translate = (0.5,2), scale = (0.5,2), shear = (0,30))
+]
 
 
 def preparing_cityscapes(dataset_add, dest, output_shape:tuple = (256, 256), mode = "train", data_set_mode = "labelIds"):
-
 
     count_img = 0
     count_mask = 0
     save_img_add = os.path.join(dest, "images", mode)
     save_mask_add = os.path.join(dest, "masks", mode)
     add = os.path.join(dataset_add, mode, "*")
-    final_image_dim = output_shape
+    final_image_dim = eval(output_shape)
+
+    print(save_img_add)
+    if not os.path.exists(save_img_add):
+        os.makedirs(save_img_add)
+
+
+    if not os.path.exists(save_mask_add):
+        os.makedirs(save_mask_add)
 
 
     for folder in glob.iglob(add):
@@ -24,12 +75,12 @@ def preparing_cityscapes(dataset_add, dest, output_shape:tuple = (256, 256), mod
 
             img = Image.open(img_add)
             input_image_shape = np.array(img).shape
-            transforms.Resize(final_image_dim)(img).save(save_img_add + "{}.jpg".format(str(count_img).zfill(6)))
+            transforms.Resize(final_image_dim)(img).save(os.path.join(save_img_add, "{}.jpg".format(str(count_img).zfill(6))))
             count_img += 1
             img = transforms.FiveCrop((int(input_image_shape[0]/2), int(input_image_shape[1]/2)))(img)
 
             seg_mask = Image.open(gtFine)
-            transforms.Resize(final_image_dim)(seg_mask).save(save_mask_add + "{}.jpg".format(str(count_mask).zfill(6)))
+            transforms.Resize(final_image_dim)(seg_mask).save(os.path.join(save_mask_add, "{}.jpg".format(str(count_mask).zfill(6))))
             count_mask += 1
             seg_mask = transforms.FiveCrop((int(input_image_shape[0]/2), int(input_image_shape[1]/2)))(seg_mask)
 
@@ -40,26 +91,26 @@ def preparing_cityscapes(dataset_add, dest, output_shape:tuple = (256, 256), mod
                     transf.__new_seed__()
 
                 tmp_img = transforms.Resize(final_image_dim)(img[i])
-                tmp_img.save(save_img_add + "{}.jpg".format(str(count_img).zfill(6)))
+                tmp_img.save(os.path.join(save_img_add, "{}.jpg".format(str(count_img).zfill(6))))
                 count_img += 1
-                transf(tmp_img).save(save_img_add + "{}.jpg".format(str(count_img).zfill(6)))
+                transf(tmp_img).save(os.path.join(save_img_add, "{}.jpg".format(str(count_img).zfill(6))))
                 count_img += 1
 
                 tmp_mask = transforms.Resize(final_image_dim)(seg_mask[i])
-                tmp_mask.save(save_mask_add + "{}.jpg".format(str(count_mask).zfill(6)))
+                tmp_mask.save(os.path.join(save_mask_add, "{}.jpg".format(str(count_mask).zfill(6))))
 
                 count_mask += 1
                 if isinstance(transf, RandomAffine):
-                    transf(tmp_mask).save(save_mask_add + "{}.jpg".format(str(count_mask).zfill(6)))
+                    transf(tmp_mask).save(os.path.join(save_mask_add, "{}.jpg".format(str(count_mask).zfill(6))))
 
                     count_mask += 1
                 else:
-                    tmp_mask.save(save_mask_add + "{}.jpg".format(str(count_mask).zfill(6)))
+                    tmp_mask.save(os.path.join(save_mask_add, "{}.jpg".format(str(count_mask).zfill(6))))
                     count_mask += 1
 
 
 def main(arg):
-    preparing_cityscapes(dataset_add = arg[0], dest = arg[1], output_shape:tuple = arg[2], mode = arg[3], data_set_mode = [4])
+    preparing_cityscapes(dataset_add = arg[1], dest = arg[2], output_shape = arg[3], mode = arg[4], data_set_mode = arg[5])
 
 if __name__ == "__main__":
     main(sys.argv)
--- preprocessing/run.sh
+++ preprocessing/run.sh
@@ -1,1 +1,1 @@
-python preprocessing_cityscapes.py "../datasets/Cityscapes/leftImg8bit" "../datasets/augmented_cityscapes" (256,256) "train" "labelIds"
+python preprocessing_cityscapes.py "../../datasets/Cityscapes/leftImg8bit" "../../datasets/augmented_cityscapes" "(256,256)" "test" "labelIds"
